4206d5664dec7311ed571ba89b329679d000f293
liyubin <liyubin@kingsoft.com>
2023-04-29 22:06:09 +0800

    feat: 添加初始化入口及相应的库的依赖

---
+++ Assets/Scripts/Game/GameLauncher.cs
@@ -0,0 +1,31 @@
+﻿using DotEngine.Core;
+using DotEngine.Core.Updater;
+using DotEngine.Frame;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameLauncher : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyHelper.Create();
+            DontDestroyHelper.AddGameObject(gameObject);
+
+            Facade.CreateInstance(() => new GameFacade());
+            UpdateProxy.Register(OnUpdate);
+        }
+
+        private void OnUpdate(float deltaTime, float unscaleDeltaTime)
+        {
+            Facade.GetInstance().Update(deltaTime, unscaleDeltaTime);
+        }
+
+        private void OnDestroy()
+        {
+            UpdateProxy.Unregister(OnUpdate);
+            Facade.DestroyInstance();
+            DontDestroyHelper.Destroy();
+        }
+    }
+}

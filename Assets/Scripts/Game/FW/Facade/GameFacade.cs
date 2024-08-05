fc7183acdbe6c2cdf78a73befc165df09e67f8a1
erick <liyubin724@163.com>
2024-08-06 01:22:09 +0800

    feat: 汇总所有的模块

---
+++ Assets/Scripts/Game/FW/Facade/GameFacade.cs
@@ -0,0 +1,20 @@
+﻿using DotEngine.Events;
+using DotEngine.FW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.FW
+{
+    public class GameFacade : Facade
+    {
+        protected override void OnInitializeDispatcher()
+        {
+            var evtMgr = EventManager.GetInstance();
+
+            base.OnInitializeDispatcher();
+        }
+    }
+}
